8d29cf7932aae26023037220d3144a487d0699fd
kade-github <26305836+Kade-github@users.noreply.github.com>
2025-10-02 11:16:45 -0700

    Implement imgui

--- EncounterMaster.cpp
+++ EncounterMaster.cpp
@@ -2,6 +2,10 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL.h>
 
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_sdl3.h"
+#include "imgui/backends/imgui_impl_sdlrenderer3.h"
+
 struct ToolState {
   SDL_Window* window;
   SDL_Renderer* renderer;
@@ -45,10 +49,22 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 
   SDL_SetRenderVSync(state->renderer, -1);  // Enable VSync if available
 
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+
+  ImGui::StyleColorsDark();
+
+  ImGui_ImplSDL3_InitForSDLRenderer(state->window, state->renderer);
+  ImGui_ImplSDLRenderer3_Init(state->renderer);
+
   return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
+
+  ImGui_ImplSDL3_ProcessEvent(event);
+
   if (event->type == SDL_EVENT_QUIT) {
     return SDL_APP_SUCCESS;  // Exit the application
   }
@@ -56,7 +72,25 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 }
 
 SDL_AppResult SDL_AppIterate(void* appstate) { 
-    return SDL_APP_CONTINUE;
+  SDL_Renderer* renderer = ((ToolState*)appstate)->renderer;
+  ImGuiIO& io = ImGui::GetIO();
+  static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    
+  ImGui_ImplSDLRenderer3_NewFrame();
+  ImGui_ImplSDL3_NewFrame();
+  ImGui::NewFrame();
+
+  ImGui::ShowDemoWindow();
+  ImGui::Render();
+  SDL_SetRenderScale(renderer, io.DisplayFramebufferScale.x,
+                     io.DisplayFramebufferScale.y);
+  SDL_SetRenderDrawColorFloat(renderer, clear_color.x, clear_color.y,
+                              clear_color.z, clear_color.w);
+  SDL_RenderClear(renderer);
+  ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+  SDL_RenderPresent(renderer);
+
+  return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result) {
